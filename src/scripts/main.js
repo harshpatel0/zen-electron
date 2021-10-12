1567cfcd421fdf83956199eface46960f0028c58
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2021-10-12 18:10:39 +0300

    Rewritten main.js to make the code not repeating for each type of timer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,111 +1,96 @@
-var timerWorker = new Worker("scripts/timer.js");
+const defaultFocusTime = 1500;
+const defaultShortBreakTime = 300;
+const defaultLongBreakTime = 900;
+
+const timerWorker = new Worker("scripts/timer.js");
+
+var sentNotification = false;
 
 var timer = new Vue({
   el: "#timerComponent",
   data: {
-    countdown: "00:00",
+    // Data
+    countdown: "0:00",
     status: "Start a new topic, project or what have you",
     description: "Press one of these button to start the timer",
+
+    // Timings
+    focusTime: 1, // 25 Minutes, default is 1500
+    shortBreakTime: 300, // 5 Minutes, default is 300
+    longBreakTime: 900, // 15 Minutes, default is 900
   },
 
   methods: {
-    // Function for the Focus Time thingi
-    startFocus: function () {
-      const focusTime = 1500;
+    startTimer: function (type) {
+      if (type == 0) {
+        this.status = "Time to focus!";
+        this.description =
+          "This is the best time to start what you need to get done as you are more focused after breaks or when starting to focus";
+        var time = this.focusTime;
+      }
+      if (type == 1) {
+        this.status = "Time to take a short break!";
+        this.description =
+          "Do some stretches or some exercises, or just cool your mind down";
+        var time = this.shortBreakTime;
+      }
+      if (type == 2) {
+        this.status = "Time to take a nice long break!";
+        this.description =
+          "Sometimes you need a longer break, use this time to get something done, if you are going to use your phone, don't get distracted by it";
+        var time = this.longBreakTime;
+      }
 
-      var tempTime = focusTime;
-      this.status = "Time to focus!";
-      this.description =
-        "This is the best time to start what you need to get done as you are more focused after breaks or when starting to focus";
-
-      timerWorker.postMessage(1500);
-      tempTime = 1500;
-
-      // Once it gets the message back, it subtracts the time and sends it back as well as
-      // updating the timer on screen
+      timerWorker.postMessage(time);
 
       countdown = timerWorker.onmessage = function (oEvent) {
-        if (tempTime == 0) {
-          timerWorker.postMessage(tempTime);
+        if (time == 0) {
+          timerWorker.postMessage(time);
         } else {
-          tempTime = tempTime - 1;
-          timerWorker.postMessage(tempTime);
+          time = time - 1;
+          timerWorker.postMessage(time);
         }
 
         if (oEvent.data == "0:00") {
-          Push.create("Focus Time is over", {
-            body: "Focus time is over, go back to the app to start your short break, if this is your 4th Focus Time, start a long break",
-            onclick: function () {
-              window.focus();
-              this.close();
-            },
-          });
-        }
+          // YandereSim Code moment right here
 
-        timer.countdown = oEvent.data;
-      };
-    },
+          if (sentNotification == true) {
+            var temp = "temp";
+          } else {
+            if (type == 0) {
+              console.log(sentNotification);
+              sentNotification = true;
+              console.log(sentNotification);
 
-    // Function for the short break thingi
-    startShortBreak: function () {
-      const shortBreak = 300;
+              Push.create("Focus Time is over", {
+                body: "Focus time is over, go back to the app to start your short break, if this is your 4th Focus Time, start a long break",
+                onclick: function () {
+                  window.focus();
+                  this.close();
+                },
+              });
+            } else if (type == 1) {
+              sentNotification = true;
 
-      var tempTime = shortBreak;
-      this.status = "Time to take a short break!";
-      this.description =
-        "Do some stretches or some exercises, or just cool your mind down";
+              Push.create("Short Break is over", {
+                body: "Your short break is over, go back to the app to start your Focus Time",
+                onclick: function () {
+                  window.focus();
+                  this.close();
+                },
+              });
+            } else if (type == 2) {
+              sentNotification = true;
 
-      timerWorker.postMessage(300);
-      tempTime = 300;
-
-      countdown = timerWorker.onmessage = function (oEvent) {
-        if (tempTime == 0) {
-          timerWorker.postMessage(tempTime);
-        } else {
-          tempTime = tempTime - 1;
-          timerWorker.postMessage(tempTime);
-        }
-
-        if (oEvent.data == "0:00") {
-          Push.create("Short Break is over", {
-            body: "Your short break is over, go back to the app to start your Focus Time",
-            onclick: function () {
-              window.focus();
-              this.close();
-            },
-          });
-        }
-
-        timer.countdown = oEvent.data;
-      };
-    },
-
-    // Function for the long break thingi
-    startLongBreak: function () {
-      const longBreak = 900;
-      this.status = "Time to take a nice long break!";
-      this.description =
-        "Sometimes you need a longer break, use this time to get something done, if you are going to use your phone, don't get distracted by it";
-
-      timerWorker.postMessage(900);
-      tempTime = 900;
-
-      countdown = timerWorker.onmessage = function (oEvent) {
-        if (tempTime == 0) {
-          timerWorker.postMessage(tempTime);
-        } else {
-          tempTime = tempTime - 1;
-          timerWorker.postMessage(tempTime);
-        }
-
-        if (oEvent.data == "0:00") {
-          Push.create("Long Break is over", {
-            body: "Your long break is over, go back to the app to start your Focus Time",
-            onclick: function () {
-              window.focus();
-              this.close();
-            },
-          });
+              Push.create("Long Break is over", {
+                body: "Your long break is over, go back to the app to start your Focus Time",
+                onclick: function () {
+                  window.focus();
+                  this.close();
+                },
+              });
+            }
+          }
         }
 
         timer.countdown = oEvent.data;
